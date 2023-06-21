64f534f7bfc320c74d35f8d7885ab01e89cbdbf0
Erik Johnson <erik@erikaugust.com>
2023-06-21 14:05:22 -0400

    M2-1, M2-2, M2-3: Adds routes for sign up, sign in, and retrieving current user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
+const passport = require('passport');
+
+const User = require('../../shared/user');
+const JWT = require('../../shared/jwt');
+const Password = require('../../shared/password');
 
 /**
  * @route POST api/users
@@ -8,13 +13,93 @@ const createError = require('http-errors');
  */
 router.post('/', async (req, res, next) => {
   const user = req.body.user;
+  const { email, password } = user;
+
+  // Transforms to lower case and removes all white space:
+  const username = User.format(user.username);
+
+  if (!email) return next(createError(400, 'Email is not set'));
+  if (!username) return next(createError(400, 'Username is not set'));
+  if (!password) return next(createError(400, 'Password is not set'));
+
+    // Check password length and strength:
+    if (Password.strength(password).id < 1) {
+      return next(createError(400, 'Password is not strong enough'));
+    }
+  
+    // Check username:
+    const usernameExists = await User.usernameExists(username);
+    if (usernameExists) return next(createError(401, 'The requested username is unavailable'));
+  
+    // Check email:
+    const emailExists = await User.emailExists(email);
+    if (emailExists) return next(createError(401, 'The email address is already in use'));
+
+    try {
+      // Create user and return:
+      const hashed = await Password.generateHash(password);
+      const user = await User.create(username, email, hashed);
+  
+      user.token = JWT.generate(
+        { id: user.id, email: user.email }, '60d'
+      );
+      return res.json({ user: {
+        id: user.id, email: user.email, token: user.token, username: user.username
+      } });
+    } catch (error) {
+      return next(createError(500, error.message));
+    }
+});
+
+/**
+ * @route POST api/users/me
+ * @author Ease <ease@machinestates.com>
+ */
+router.post('/me', async (req, res, next) => {
+  const body = req.body.user;
+  const { emailusername, password } = body;
+
+  if (!emailusername) return next(createError(400));
+  if (!password) return next(createError(400));
 
   try {
-    user.token = '123456789';
-    user.id = 1;
-    return res.json({ user: {
-      id: user.id, email: user.email, token: user.token, username: user.username
-    } });
+    // Get user:
+    const user = await User.getByEmailOrUsername(emailusername);
+
+    // If user does not exist:
+    if (!user) return next(createError(404, 'Incorrect username/email and/or password.'));
+
+    const hash = user.password;
+    if (await Password.verify(password, hash)) {
+      // Success:
+      user.token = JWT.generate({ id: user.id, email: user.email }, '60d');
+      return res.json({ user: {
+        id: user.id, email: user.email, token: user.token, username: user.username
+      } });
+    } else {
+      return next(createError(404, 'Incorrect username/email and/or password.'));
+    }
+    } catch (error) {
+      return next(error);
+    }
+});
+
+/**
+ * @route GET /api/users/me
+ * @author Ease <ease@machinestates.com>
+ */
+router.get('/me', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+  const userId = req.user.id;
+  if (!userId) return next(createError(401));
+
+  try {
+    const user = await User.getById(userId);
+    return res.json({
+      user: {
+        ...user.dataValues,
+        token: req.headers.authorization.replace('Bearer ', ''),
+      }
+    });
   } catch (error) {
     return next(createError(500, error.message));
   }
---
+++ routes/api/users.spec.js
@@ -0,0 +1,143 @@
+require('dotenv').config();
+
+const app = require('../../app');
+const supertest = require('supertest');
+const request = supertest(app);
+
+const User = require('../../shared/user');
+const JWT = require('../../shared/jwt');
+
+describe('POST /api/users', () => {
+  test('should return 200 success when valid user is created', async () => {
+    const user = { username: 'test1337', email: 'eriktest@machinestates.com', password: 'password1' };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ user })
+    await User.remove(user.username);
+    expect(response.status).toBe(200);
+    expect(response.body.user.token).toBeTruthy();
+    expect(response.body.user.username).toBeTruthy();
+  });
+
+  test('should return 500 failure when Internal Server Error', async () => {
+    jest
+        .spyOn(User, 'create')
+        .mockImplementationOnce(() => {
+    throw new Error('Internal Server Error');
+    });
+
+    const user = { username: 'test1337', email: 'eriktest@machinestates.com', password: 'password1' };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(500);
+  });
+
+  test('should return 400 failure when username is not sent', async () => {
+    const body = { user: { email: 'eriktest@machinestates.com', password: 'test' } };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Username is not set' });
+  });
+
+  test('should return 400 failure when email is not sent', async () => {
+    const body = { user: { username: 'test1337', password: 'test' } };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Email is not set' });
+  });
+
+  test('should return 400 failure when password is not sent', async () => {
+    const body = { user: { username: 'test1337', email: 'eriktest@machinestates.com' } };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Password is not set' });
+  });
+
+  test('should return 400 failure when password is not strong enough', async () => {
+    const body = { user: { username: 'test1337', email: 'eriktest@machinestates.com', password: 'love' } };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Password is not strong enough' });
+  });
+
+  test('should return 401 failure if username exist or is reserved', async () => {
+    const body = { user: { username: 'test', email: 'eriktest@machinestates.com', password: 'password1' } };
+    const response = await request.post('/api/users')
+        .set('Accept', 'application/json')
+        .send(body);
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({ message: 'The requested username is unavailable' });
+  });
+});
+
+describe('POST /api/users/me', () => {
+  test('should return 200 success when valid credentials (email) are passed', async () => {
+    const user = { emailusername: 'erik@erikaugust.com', password: process.env.ENTRY_PASSWORD };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(200);
+    expect(response.body.user.token).toBeTruthy();
+    expect(response.body.user.email).toBeTruthy();
+  });
+
+  test('should return 500 failure when error is thrown', async () => {
+    jest
+        .spyOn(JWT, 'generate')
+        .mockImplementationOnce(() => {
+        throw new Error('Internal Server Error');
+    });
+    const user = { emailusername: 'erik@erikaugust.com', password: process.env.ENTRY_PASSWORD };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(500);
+  });
+
+  test('should return 400 failure when password is not passed', async () => {
+    const user = { emailusername: 'erik@erikaugust.com' };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(400);
+  });
+
+  test('should return 400 failure when username is not passed', async () => {
+    const user = { password: 'eaj' };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(400);
+  });
+
+  test('should return 404 failure when username does not exist', async () => {
+    const user = { emailusername: 'test1337', password: 'password1' };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(404);
+  });
+
+  test('should return 404 failure when password is incorrect', async () => {
+    const user = { emailusername: 'erik@erikaugust.com', password: 'password1' };
+    const response = await request.post('/api/users/me')
+        .set('Accept', 'application/json')
+        .send({ user })
+    expect(response.status).toBe(404);
+});
+
+});
